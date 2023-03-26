95b3a7fa7dbec856dcc1ca732eab3dc31cfad04c
Alex Panasko <a.panasko@bk.ru>
2023-03-26 21:31:13 +0500

    Correct manual tests

--- src/test/java/ru/netology/javaqa/cyclesmvn/services/VacationTest.java
+++ src/test/java/ru/netology/javaqa/cyclesmvn/services/VacationTest.java
@@ -12,9 +12,9 @@ public class VacationTest {
 
         int expected = 3;
         int income = 10_000;
-        int expences = 3_000;
+        int expenses = 3_000;
         int threshold = 20_000;
-        int actual = service.calculate(income, expences, threshold);
+        int actual = service.calculate(income, expenses, threshold);
 
         Assertions.assertEquals(expected, actual);
 
@@ -26,9 +26,9 @@ public class VacationTest {
 
         int expected = 2;
         int income = 100_000;
-        int expences = 60_000;
+        int expenses = 60_000;
         int threshold = 150_000;
-        int actual = service.calculate(income, expences, threshold);
+        int actual = service.calculate(income, expenses, threshold);
 
         Assertions.assertEquals(expected, actual);
     }
