c01347eba2c399c750036aa71e05d1070f8ed56c
Alex Panasko <a.panasko@bk.ru>
2023-03-26 21:56:21 +0500

    Parametrized tests

--- src/test/java/ru/netology/javaqa/cyclesmvn/services/VacationTest.java
+++ src/test/java/ru/netology/javaqa/cyclesmvn/services/VacationTest.java
@@ -1,35 +1,38 @@
 package ru.netology.javaqa.cyclesmvn.services;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 //import ru.netology.javaqa.cyclesmvn.services.Vacation;
 
 public class VacationTest {
 
-    @Test
-    public void testSumVacationLow() {
+    @ParameterizedTest
+    @CsvFileSource(files="src/test/resources/vacation.csv")
+    public void testSumVacationLow(int expected, int income, int expenses, int threshold) {
         Vacation service = new Vacation();
 
-        int expected = 3;
-        int income = 10_000;
-        int expenses = 3_000;
-        int threshold = 20_000;
+//        int expected = 3;
+//        int income = 10_000;
+//        int expenses = 3_000;
+//        int threshold = 20_000;
         int actual = service.calculate(income, expenses, threshold);
 
         Assertions.assertEquals(expected, actual);
 
     }
 
-    @Test
-    public void testSumVacationHigh() {
-        Vacation service = new Vacation();
-
-        int expected = 2;
-        int income = 100_000;
-        int expenses = 60_000;
-        int threshold = 150_000;
-        int actual = service.calculate(income, expenses, threshold);
-
-        Assertions.assertEquals(expected, actual);
-    }
+//    @Test
+//    public void testSumVacationHigh() {
+//        Vacation service = new Vacation();
+//
+//        int expected = 2;
+//        int income = 100_000;
+//        int expenses = 60_000;
+//        int threshold = 150_000;
+//        int actual = service.calculate(income, expenses, threshold);
+//
+//        Assertions.assertEquals(expected, actual);
+//    }
 }
