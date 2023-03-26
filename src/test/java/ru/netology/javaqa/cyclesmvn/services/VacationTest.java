88b04a99a3aaec6e8b8e461caa61d184db973d56
Alex Panasko <a.panasko@bk.ru>
2023-03-26 21:14:51 +0500

    Manual tests

---
+++ src/test/java/ru/netology/javaqa/cyclesmvn/services/VacationTest.java
@@ -0,0 +1,35 @@
+package ru.netology.javaqa.cyclesmvn.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+//import ru.netology.javaqa.cyclesmvn.services.Vacation;
+
+public class VacationTest {
+
+    @Test
+    public void testSumVacationLow() {
+        Vacation service = new Vacation();
+
+        int expected = 3;
+        int income = 10_000;
+        int expences = 3_000;
+        int threshold = 20_000;
+        int actual = service.calculate(income, expences, threshold);
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testSumVacationHigh() {
+        Vacation service = new Vacation();
+
+        int expected = 2;
+        int income = 100_000;
+        int expences = 60_000;
+        int threshold = 150_000;
+        int actual = service.calculate(income, expences, threshold);
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
